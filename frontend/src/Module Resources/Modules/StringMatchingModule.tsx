71cd055c8bdd669fb6ebb2aa6367499b128ffb8e
Alex Egorchatov <alex.exorchatov@gmail.com>
2023-01-14 19:36:31 -0800

    Worked on String Matching module.

--- frontend/src/Module Resources/Modules/StringMatchingModule.tsx
+++ frontend/src/Module Resources/Modules/StringMatchingModule.tsx
@@ -1,15 +1,81 @@
+/**@jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
 import { IModule } from '../Module';
 import { ModulePlaceholder } from '../ModulePlaceHolder';
 
+export enum CharacterState {
+  Unselected = 0,
+  Selected = 1,
+  Found = 2,
+}
+
+interface Props {
+  character: string;
+  isSelected?: boolean;
+}
+
+const SearchableCharacter = ({ character, isSelected = false }: Props) => {
+  return (
+    <div
+      css={css`
+        ${isSelected === true && `background-color: yellow`}
+      `}
+    >
+      {character}
+    </div>
+  );
+};
+
 export const StringMatchingModule = ({ title }: IModule) => {
+  const test = [
+    <SearchableCharacter character={'a'} />,
+    <SearchableCharacter character={'b'} />,
+    <SearchableCharacter character={'b'} />,
+    <SearchableCharacter character={'a'} />,
+  ];
+
   const handleModuleMouseEnter = () => {};
   const handleModuleMouseLeave = () => {};
 
   return (
     <div onMouseEnter={handleModuleMouseEnter} onMouseLeave={handleModuleMouseLeave}>
-      <ModulePlaceholder title={title}>
-        <div>TODO String Matching</div>
+      <ModulePlaceholder title={title} isGrid={true}>
+        <div
+          css={css`
+            font-size: 24px;
+            color: #ffffff;
+            display: flex;
+            margin: 40px 0 0 10px;
+          `}
+        >
+          Input:
+          <div
+            css={css`
+              margin-left: 5px;
+              display: flex;
+            `}
+          >
+            {test}
+          </div>
+        </div>
+        <div
+          css={css`
+            font-size: 24px;
+            color: #ffffff;
+            display: flex;
+            margin: 10px;
+          `}
+        >
+          Search:
+          <div
+            css={css`
+              margin-left: 5px;
+            `}
+          >
+            search result
+          </div>
+        </div>
       </ModulePlaceholder>
     </div>
   );
