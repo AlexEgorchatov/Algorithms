37f8edd7e24a630a199914b583663e74516ae947
Alex Egorchatov <alex.exorchatov@gmail.com>
2023-05-29 21:44:54 -0700

    Refactored Header component.

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -19,32 +19,120 @@ const HeaderStyle = css`
   }
 `;
 
-const HeaderMenuButton = () => {
+const HeaderMainComponent = ({ data }: Props) => {
   return (
     <div
       css={css`
-        &,
-        &::after,
-        &::before {
-          box-sizing: border-box;
-          position: relative;
-          width: 25px;
-          height: 2px;
-          border-radius: 3px;
-          color: ${mainFontColor};
-          background: currentColor;
-        }
-        &::after,
-        &::before {
-          content: '';
-          position: absolute;
-          top: -8px;
-        }
-        &::after {
-          top: 8px;
+        @media (max-width: 450px) {
+          display: none;
         }
       `}
-    ></div>
+    >
+      <Link css={HeaderStyle} style={{ marginRight: '10px' }} to="" reloadDocument={true}>
+        Home
+      </Link>
+      <div
+        css={css`
+          position: relative;
+          display: inline-block;
+          vertical-align: super;
+          margin-right: 10px;
+          :hover {
+            & > div {
+              display: block;
+            }
+            & > button {
+              color: ${headerItemHovered};
+            }
+          }
+        `}
+      >
+        <button
+          css={css`
+            background-color: transparent;
+            color: ${mainFontColor};
+            font-size: 28px;
+            border: none;
+            cursor: pointer;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+              Helvetica Neue, sans-serif;
+          `}
+        >
+          Modules
+        </button>
+        <div
+          css={css`
+            display: none;
+            position: absolute;
+            background-color: #f1f1f1;
+            z-index: 1;
+          `}
+        >
+          {data.map((item, index) => (
+            <Link
+              css={css`
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block;
+                text-decoration: 'none';
+                :hover {
+                  background-color: #ddd;
+                }
+              `}
+              key={index}
+              to={item.link}
+              reloadDocument={true}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </div>
+      </div>
+      <Link css={HeaderStyle} to="#" reloadDocument={true}>
+        About
+      </Link>
+    </div>
+  );
+};
+
+const HeaderMenuButton = () => {
+  const barStyle = css`
+    width: 30px;
+    height: 4px;
+    background-color: ${mainFontColor};
+    margin: 6px 0;
+  `;
+
+  return (
+    <div
+      css={css`
+        position: relative;
+        display: inline-block;
+        :hover {
+          & > div.bar {
+            background-color: ${headerItemHovered};
+          }
+        }
+        @media (min-width: 4501px) {
+          display: none;
+        }
+      `}
+    >
+      <div className="bar" css={barStyle}></div>
+      <div className="bar" css={barStyle}></div>
+      <div className="bar" css={barStyle}></div>
+      <div
+        className="test"
+        css={css`
+          display: none;
+        `}
+      >
+        <a>Test1</a>
+        <a>Test1</a>
+        <a>Test1</a>
+      </div>
+    </div>
   );
 };
 
@@ -56,7 +144,7 @@ export const Header = ({ data }: Props) => {
         box-sizing: border-box;
         top: 0;
         width: 100%;
-        height: 42px;
+        height: 50px;
         min-width: 450px;
         display: flex;
         align-items: center;
@@ -66,95 +154,8 @@ export const Header = ({ data }: Props) => {
         z-index: 999;
       `}
     >
-      <div
-        css={css`
-          @media (max-width: 450px) {
-            display: none;
-          }
-        `}
-      >
-        <Link css={HeaderStyle} style={{ marginRight: '10px' }} to="" reloadDocument={true}>
-          Home
-        </Link>
-        <div
-          css={css`
-            position: relative;
-            display: inline-block;
-            vertical-align: super;
-            margin-right: 10px;
-            :hover {
-              & > div {
-                display: block;
-              }
-              & > button {
-                color: ${headerItemHovered};
-              }
-            }
-          `}
-        >
-          <button
-            css={css`
-              background-color: transparent;
-              color: ${mainFontColor};
-              font-size: 28px;
-              border: none;
-              cursor: pointer;
-              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                Helvetica Neue, sans-serif;
-            `}
-          >
-            Modules
-          </button>
-          <div
-            css={css`
-              display: none;
-              position: absolute;
-              background-color: #f1f1f1;
-              z-index: 1;
-            `}
-          >
-            {data.map((item, index) => (
-              <Link
-                css={css`
-                  color: black;
-                  padding: 12px 16px;
-                  text-decoration: none;
-                  display: block;
-                  text-decoration: 'none';
-                  :hover {
-                    background-color: #ddd;
-                  }
-                `}
-                key={index}
-                to={item.link}
-                reloadDocument={true}
-              >
-                {item.title}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <Link css={HeaderStyle} to="#" reloadDocument={true}>
-          About
-        </Link>
-      </div>
-
-      <div
-        css={css`
-          display: contents;
-          height: 420px;
-          :hover {
-            & > div {
-              color: ${headerItemHovered};
-            }
-          }
-          @media (min-width: 451px) {
-            display: none;
-          }
-        `}
-      >
-        <HeaderMenuButton></HeaderMenuButton>
-      </div>
+      <HeaderMainComponent data={data} />
+      <HeaderMenuButton />
 
       <Link css={HeaderStyle} style={{ fontSize: '18px' }} to="*" reloadDocument={true}>
         Sign In
