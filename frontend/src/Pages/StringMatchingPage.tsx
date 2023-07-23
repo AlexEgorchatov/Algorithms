032bb9a519e0bd239ee505610fad8f16ec30f83f
Alex Egorchatov <alex.exorchatov@gmail.com>
2023-07-23 16:38:24 -0700

    Worked on implementing String Matching page. Added string matching algorithms list.

--- frontend/src/Pages/StringMatchingPage.tsx
+++ frontend/src/Pages/StringMatchingPage.tsx
@@ -1,6 +1,164 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /**@jsxImportSource @emotion/react */
-import React from 'react';
+import { css } from '@emotion/react';
+import { useEffect } from 'react';
+import { headerItemHovered, mainFontColor } from '../Resources/Colors';
+import { StringMatchingData, stringMatchingAlgorithms } from '../Resources/String Matching Page Resources/StringMatchingData';
+import { StringMatchingAlgorithmBase, StringMatchingAlgorithmEnum } from '../Resources/Algorithms/AlgorithmBase';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState } from '../Store/Store';
+import { NaivePatternMatching } from '../Resources/Algorithms/StringMatchingAlgorithms';
+import { updatingSelectedStringMatchingAlgorithmState } from '../Store/String Matching Page/StringMatchingPageStateManagement';
+import { updatingWindowHeightStateAction, updatingWindowWidthStateAction } from '../Store/Shared/WindowStateManagement';
+
+export let selectedStringMatchingAlgorithm: StringMatchingAlgorithmBase = new NaivePatternMatching(StringMatchingAlgorithmEnum.Naive);
+
+interface AlgorithmListProps {
+  data: StringMatchingData[];
+}
+
+interface AlgorithmProps {
+  title: string;
+  isSelected: boolean;
+  stringMatchingAlgorithm: StringMatchingAlgorithmBase;
+}
+
+const AlgorithmComponent = ({ title, isSelected, stringMatchingAlgorithm }: AlgorithmProps) => {
+  const algorithmState = useSelector((state: AppState) => state.sortingPageState);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    if (algorithmState.hasAlgorithmStarted) return;
+    selectedStringMatchingAlgorithm = stringMatchingAlgorithm;
+    dispatch(updatingSelectedStringMatchingAlgorithmState(stringMatchingAlgorithm.stringMatchingAlgorithm));
+  };
+
+  return (
+    <div
+      css={css`
+        font-size: 20px;
+        color: ${isSelected ? '' : 'white'};
+        margin-right: 10px;
+        cursor: ${algorithmState.hasAlgorithmStarted && !isSelected ? 'default' : 'pointer'};
+        opacity: ${algorithmState.hasAlgorithmStarted && !isSelected ? '0.5' : '1'};
+        :hover {
+          ${!algorithmState.hasAlgorithmStarted &&
+          `
+            color: ${!isSelected ? `${headerItemHovered}` : ''};
+          `}
+        }
+      `}
+      onClick={handleClick}
+    >
+      {title}
+    </div>
+  );
+};
+
+const AlgorithmsList = ({ data }: AlgorithmListProps) => {
+  const algorithmState = useSelector((state: AppState) => state.stringMatchingPageState);
+
+  return (
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      {data.map((algorithm, index) => (
+        <AlgorithmComponent
+          key={index}
+          title={algorithm.title}
+          isSelected={algorithm.stringMatchingAlgorithm.stringMatchingAlgorithm === algorithmState.selectedStringMatchingAlgorithm}
+          stringMatchingAlgorithm={algorithm.stringMatchingAlgorithm}
+        />
+      ))}
+    </div>
+  );
+};
+
+const SettingsComponent = () => {
+  return (
+    <div
+      css={css`
+        margin: 0px 10px;
+        height: 25%;
+        min-height: 200px;
+        display: block;
+      `}
+    >
+      <div
+        css={css`
+          height: 50px;
+          min-height: 50px;
+        `}
+      >
+        String Matching
+      </div>
+      <div
+        css={css`
+          height: 70%;
+          min-height: 118px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+        `}
+      >
+        <div
+          css={css`
+            display: flex;
+            align-items: flex-end;
+            justify-content: space-between;
+            width: 250px;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              justify-content: space-between;
+              width: 72px;
+            `}
+          >
+            {/* <ActionBar /> */}
+          </div>
+          {/* <GenerateInputComponent /> */}
+        </div>
+        <AlgorithmsList data={stringMatchingAlgorithms} />
+      </div>
+    </div>
+  );
+};
 
 export const StringMatchingPage = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      dispatch(updatingWindowWidthStateAction(window.innerWidth));
+      dispatch(updatingWindowHeightStateAction(window.outerHeight));
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <div
+      css={css`
+        color: ${mainFontColor};
+        font-size: 36px;
+        text-align: left;
+        overflow: auto;
+        height: 100%;
+      `}
+    >
+      <SettingsComponent />
+      <div
+        css={css`
+          height: 75%;
+        `}
+      ></div>
+    </div>
+  );
 };
