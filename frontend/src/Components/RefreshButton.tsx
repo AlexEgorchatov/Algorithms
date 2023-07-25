6b2719ab6fa5c29badb161b727194aea9b6d1962
Alex Egorchatov <alex.exorchatov@gmail.com>
2023-07-24 23:49:31 -0700

    Abstracted the Refresh Button component.

---
+++ frontend/src/Components/RefreshButton.tsx
@@ -0,0 +1,66 @@
+/**@jsxImportSource @emotion/react */
+import { useSelector } from 'react-redux';
+import { AppState } from '../Store/Store';
+import { css } from '@emotion/react';
+import { headerItemHovered } from '../Resources/Colors';
+
+export const RefreshButton = () => {
+  const sortingPageState = useSelector((state: AppState) => state.sortingPageState);
+
+  return (
+    <div
+      css={css`
+        align-items: center;
+        justify-content: center;
+        box-sizing: border-box;
+        position: relative;
+        display: flex;
+        transform: scale(var(--ggs, 1));
+        width: 22px;
+        height: 22px;
+        border: 2px solid;
+        border-radius: 4px;
+        color: white;
+        cursor: ${!sortingPageState.hasSortingAlgorithmStarted ? 'pointer' : 'default'};
+        opacity: ${!sortingPageState.hasSortingAlgorithmStarted ? '1' : '0.5'};
+        :hover {
+          ${!sortingPageState.hasSortingAlgorithmStarted &&
+          `
+              color: ${headerItemHovered};
+              & > div {
+                color: ${headerItemHovered};
+              }
+            `}
+        }
+      `}
+    >
+      <div
+        css={css`
+          box-sizing: border-box;
+          position: relative;
+          display: block;
+          transform: scale(var(--ggs, 1));
+          width: 12px;
+          height: 12px;
+          border: 2px solid;
+          border-right-color: transparent;
+          border-radius: 100px;
+          color: white;
+          ::before {
+            content: '';
+            display: block;
+            box-sizing: border-box;
+            position: absolute;
+            width: 6px;
+            height: 6px;
+            border-top: 2px solid;
+            border-right: 2px solid;
+            top: -3px;
+            right: -1px;
+            transform: rotate(68deg);
+          }
+        `}
+      ></div>
+    </div>
+  );
+};
