d353dbdfee824762ea7e60c343607db01e3d4ec2
AlexEgorchatov <alex.exorchatov@gmail.com>
2023-11-13 13:25:58 -0800

    Changed file to match the updated line length settings

--- frontend/src/Store/Path Finding Module/PathFindingModuleStateManagement.ts
+++ frontend/src/Store/Path Finding Module/PathFindingModuleStateManagement.ts
@@ -1,5 +1,9 @@
 import { IPathFindingCellProps } from '../../Core/Interfaces/IPathFindingCellProps';
-import { PathFindingCellActionStateEnum, PathFindingCellDraggingStateEnum, PathFindingCellStateEnum } from '../../Resources/Enumerations';
+import {
+  PathFindingCellActionStateEnum,
+  PathFindingCellDraggingStateEnum,
+  PathFindingCellStateEnum,
+} from '../../Resources/Enumerations';
 
 //#region State
 export interface PathFindingModuleState {
@@ -18,56 +22,81 @@ const initialPathFindingModuleState: PathFindingModuleState = {
   pathFindingGrid: [],
   pathFindingSelectedCellAction: PathFindingCellActionStateEnum.None,
   pathFindingSelectedCellDragging: PathFindingCellDraggingStateEnum.None,
-  pathFindingSource: { cellState: PathFindingCellStateEnum.Unselected, rowIndex: 0, columnIndex: 0, distance: 0 },
-  pathFindingDestination: { cellState: PathFindingCellStateEnum.Unselected, rowIndex: 0, columnIndex: 0, distance: 0 },
+  pathFindingSource: {
+    cellState: PathFindingCellStateEnum.Unselected,
+    rowIndex: 0,
+    columnIndex: 0,
+    distance: 0,
+  },
+  pathFindingDestination: {
+    cellState: PathFindingCellStateEnum.Unselected,
+    rowIndex: 0,
+    columnIndex: 0,
+    distance: 0,
+  },
 };
 //#endregion State
 
 //#region Actions
 const UPDATE_SELECTED_PATH_FINDING_ALGORITHM_STATE = 'updateSelectedPathFindingAlgorithmState';
-export const updateSelectedPathFindingAlgorithmState = (selectedPathFindingAlgorithm = initialPathFindingModuleState.selectedPathFindingAlgorithm) =>
+export const updateSelectedPathFindingAlgorithmState = (
+  selectedPathFindingAlgorithm = initialPathFindingModuleState.selectedPathFindingAlgorithm,
+) =>
   ({
     type: UPDATE_SELECTED_PATH_FINDING_ALGORITHM_STATE,
     selectedPathFindingAlgorithm: selectedPathFindingAlgorithm,
   }) as const;
 
 const UPDATE_PATH_FINDING_WARNING_MESSAGE_STATE = 'updatePathFindingWarningMessageState';
-export const updatePathFindingWarningMessageState = (pathFindingWarningMessage = initialPathFindingModuleState.pathFindingWarningMessage) =>
+export const updatePathFindingWarningMessageState = (
+  pathFindingWarningMessage = initialPathFindingModuleState.pathFindingWarningMessage,
+) =>
   ({
     type: UPDATE_PATH_FINDING_WARNING_MESSAGE_STATE,
     pathFindingWarningMessage: pathFindingWarningMessage,
   }) as const;
 
 const UPDATE_PATH_FINDING_GRID_STATE = 'updatePathFindingGridState';
-export const updatePathFindingGridState = (pathFindingGrid = initialPathFindingModuleState.pathFindingGrid) =>
+export const updatePathFindingGridState = (
+  pathFindingGrid = initialPathFindingModuleState.pathFindingGrid,
+) =>
   ({
     type: UPDATE_PATH_FINDING_GRID_STATE,
     pathFindingGrid: pathFindingGrid,
   }) as const;
 
 const UPDATE_PATH_FINDING_SELECTED_CELL_ACTION_STATE = 'updatePathFindingSelectedCellActionState';
-export const updatePathFindingSelectedCellActionState = (pathFindingSelectedCellAction = initialPathFindingModuleState.pathFindingSelectedCellAction) =>
+export const updatePathFindingSelectedCellActionState = (
+  pathFindingSelectedCellAction = initialPathFindingModuleState.pathFindingSelectedCellAction,
+) =>
   ({
     type: UPDATE_PATH_FINDING_SELECTED_CELL_ACTION_STATE,
     pathFindingSelectedCellAction: pathFindingSelectedCellAction,
   }) as const;
 
-const UPDATE_PATH_FINDING_SELECTED_CELL_DRAGGING_STATE = 'updatePathFindingSelectedCellDraggingState';
-export const updatePathFindingSelectedCellDraggingState = (pathFindingSelectedCellDragging = initialPathFindingModuleState.pathFindingSelectedCellDragging) =>
+const UPDATE_PATH_FINDING_SELECTED_CELL_DRAGGING_STATE =
+  'updatePathFindingSelectedCellDraggingState';
+export const updatePathFindingSelectedCellDraggingState = (
+  pathFindingSelectedCellDragging = initialPathFindingModuleState.pathFindingSelectedCellDragging,
+) =>
   ({
     type: UPDATE_PATH_FINDING_SELECTED_CELL_DRAGGING_STATE,
     pathFindingSelectedCellDragging: pathFindingSelectedCellDragging,
   }) as const;
 
 const UPDATE_PATH_FINDING_SOURCE_STATE = 'updatePathFindingSourceState';
-export const updatePathFindingSourceState = (pathFindingSource = initialPathFindingModuleState.pathFindingSource) =>
+export const updatePathFindingSourceState = (
+  pathFindingSource = initialPathFindingModuleState.pathFindingSource,
+) =>
   ({
     type: UPDATE_PATH_FINDING_SOURCE_STATE,
     pathFindingSource: pathFindingSource,
   }) as const;
 
 const UPDATE_PATH_FINDING_DESTINATION_STATE = 'updatePathFindingDestinationState';
-export const updatePathFindingDestinationState = (pathFindingDestination = initialPathFindingModuleState.pathFindingDestination) =>
+export const updatePathFindingDestinationState = (
+  pathFindingDestination = initialPathFindingModuleState.pathFindingDestination,
+) =>
   ({
     type: UPDATE_PATH_FINDING_DESTINATION_STATE,
     pathFindingDestination: pathFindingDestination,
@@ -84,7 +113,10 @@ type PathFindingModuleActions =
   | ReturnType<typeof updatePathFindingSelectedCellDraggingState>
   | ReturnType<typeof updatePathFindingSourceState>
   | ReturnType<typeof updatePathFindingDestinationState>;
-export const pathFindingModuleReducer = (state = initialPathFindingModuleState, action: PathFindingModuleActions) => {
+export const pathFindingModuleReducer = (
+  state = initialPathFindingModuleState,
+  action: PathFindingModuleActions,
+) => {
   switch (action.type) {
     case UPDATE_SELECTED_PATH_FINDING_ALGORITHM_STATE:
       return {
